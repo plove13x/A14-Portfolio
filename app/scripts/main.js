14974debeac7339fd34ccabcdbe068d3ccf270ba
Patrick Love <plove13x@gmail.com>
2014-11-27 01:59:48 -0500

    Second commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,3 +20,15 @@ function Slider(array, location, interval, fade) {
 
 var newSlider = new Slider(imageArray, '.slider-images', 2000, 0);
 
+
+
+
+$('.rollIt').hover(
+    function(){
+        // alert('*Have you ever tired of plain old Lemon-Lime or Fruit Punch and opted for a Gatorade X-Factor instead? You can thank the National Hybrid Research Team (NHRT) for that. No, we didn\'t reap the financial rewards of such a stroke of mixology brilliance - the idealists we were in high school couldn\'t have been less concerned with intellectual property rights and patents - but we got some serious traffic for a while and then all of a sudden Gatorade started producing their own "hybrids". Unfortunately the site is no longer accessible to due to an overhaul of the Tripod free hosting program, but long live the memories and keep on mixing!');
+        $('.onRoll').css({display: "block"});
+    },
+    function(){
+        $('.onRoll').css({display: "none"});
+    }
+);
