f0209bae6f24d3aab363f78575a255bf13e7f8d2
Patrick Love <plove13x@gmail.com>
2014-12-16 18:19:16 -0500

    Adds 5th fun fact

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -113,10 +113,12 @@ $(document).ready(function(){
     //  }); 
 
     var funFacts = [
-    'Some might say in my younger years I could be a bit of a problem child, but I wasn\'t the <em>Problem Child</em>. However, since some of the movie\'s birthday party scene was filmed at our house, a keen eye will notice young me in a lion costume. Any Oscar noms went to my Mom though for her role as "woman gets pinned on the ass". Check it out on YouTube: <a target="_blank" href="https://www.youtube.com/watch?v=KhAg5po8InY"><em>Problem Child</em> party scene clip</a>',
-    '*At the top of the page I mentioned making my first website (NHRT) in high school...Well, have you ever gotten bored with plain old Lemon-Lime or Fruit Punch and opted for a Gatorade X-Factor instead? You can thank the National Hybrid Research Team (NHRT) for that. No, we didn\'t reap the financial rewards of such a stroke of mixology brilliance - the idealists we were in high school couldn\'t have been less concerned with intellectual property rights and patents - but we got some serious traffic for a while and then all of a sudden Gatorade started producing their own "hybrids". Unfortunately, the site (www.areyoumixing.com) is no longer accessible due to an overhaul of the Tripod free hosting program, but long live the memories and keep on mixing!',
+
+    '<span class="purps2">*</span>At the top of the page I mentioned making my first website (NHRT) in high school...Well, have you ever gotten bored with plain old Lemon-Lime or Fruit Punch and opted for a Gatorade X-Factor instead? You can thank NHRT (National Hybrid Research Team) for that. No, we didn\'t reap the financial rewards of such a stroke of mixology brilliance - the idealists we were in high school couldn\'t have been less concerned with intellectual property rights and patents - but we got some serious traffic for a while and then all of a sudden Gatorade started producing their own "hybrids". Unfortunately, the site (www.areyoumixing.com) is no longer accessible due to an overhaul of the Tripod free hosting program, but long live the memories and keep on mixing!',
     'Speaking of high school, I (thanks to some timely self-deprecating humor) was elected as both junior and senior class president of my high school class and gave our graduation speech. I also served as Managing Editor of our high school newspaper that won 1st place at the <em>Dallas Morning News\’</em> Journalism Day competition. Dork!',
-    'I love basketball and once tried out for an American Basketball Association team. Ultimately I wasn\'t offered a contract, but on the plus side, I was invited back for another tryout. On the not-so-plus side, one large man who was already under contract chose to refer to me exclusively as "Little Man". This was the first and only time in my life this has happened.'
+    'I love basketball and once tried out for an American Basketball Association team. Ultimately I wasn\'t offered a contract, but on the plus side, I was invited back for another tryout. On the not-so-plus side, one large man who was already under contract chose to refer to me exclusively as "Little Man". This was the first and only time in my life this has happened.',
+    'Some might say in my younger years I could be a bit of a problem child, but I wasn\'t the <em>Problem Child</em>. However, since some of the movie\'s birthday party scene was filmed at our house, a keen eye will notice young me in a lion costume. Any Oscar noms went to my Mom though for her role as "woman gets pinned on the ass". Check it out on YouTube: <a target="_blank" href="https://www.youtube.com/watch?v=KhAg5po8InY"><em>Problem Child</em> party scene clip</a>',
+    'My junior high (5th-8th grade) holds an annual skoolwide spelling bee. I akshally won it twyse (6th and 8th grade)!'
     ];
 
     var counterFF = 0;
@@ -130,8 +132,8 @@ $(document).ready(function(){
 
         var fFP = $('.fFact p:first');
         fFP.fadeOut(600, function() {
-            if (counterFF >= 4) { counterFF = 0 }
-            if (counterFF < 0) { counterFF = 3 }
+            if (counterFF >= 5) { counterFF = 0 }
+            if (counterFF < 0) { counterFF = 4 }
             fFP.html(funFacts[counterFF]);
             fFP.fadeIn(3000);
         });
